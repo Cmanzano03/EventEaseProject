c45459e257efa902ff6aa98dc064c9d4e4da21b5
Carlos <cmanzanoo@alumnos.unex.es>
2024-07-16 11:10:15 +0200

    Todos los cambios al proyecto de configuracion añadidos

--- Event_Ease_Project/web/scripts/matrixCount.js
+++ Event_Ease_Project/web/scripts/matrixCount.js
@@ -27,34 +27,32 @@
 
 
 
-function printResult( stringa ) {
-   
+function printResult(stringa) {
+
 	// variabili di funzione
-	
-	
-		var result = stringa.split("@");
-		var resElement;
-		for(var i =0; i<result.length; i++)
-			{
-			resElement = document.getElementById("r"+i);
-			resElement.value = result[i];
-			}
-		// Ottengo la lista delle sezioni del docuemento
-		
-		
-		
-		
-		for(var i=0; i<8; i++)
-			{
-				var elementA = document.getElementById("a"+i);
-				var elementB = document.getElementById("b"+i);
-				elementA.value = '';
-				elementB.value = '';
-			}
-		
-	
-	
-     
+
+
+	var result = stringa.split("@");
+	var resElement;
+	for (var i = 0; i < result.length; i++) {
+		resElement = document.getElementById("r" + i);
+		resElement.value = result[i];
+	}
+	// Ottengo la lista delle sezioni del docuemento
+
+
+
+
+	for (var i = 0; i < 8; i++) {
+		var elementA = document.getElementById("a" + i);
+		var elementB = document.getElementById("b" + i);
+		elementA.value = '';
+		elementB.value = '';
+	}
+
+
+
+
 
 }// parsificaJSON()
 
@@ -65,30 +63,30 @@ function printResult( stringa ) {
 /*
  * Funzione di callback
  */
-function callback( theXhr ) {
+function callback(theXhr) {
 
-	
-	if ( theXhr.readyState === 2 ) {
-	    	//theElement.innerHTML = "Richiesta inviata...";
+
+	if (theXhr.readyState === 2) {
+		//theElement.innerHTML = "Richiesta inviata...";
 	}// if 2
-	else if ( theXhr.readyState === 3 ) {
-    	//	theElement.innerHTML = "Ricezione della risposta...";
+	else if (theXhr.readyState === 3) {
+		//	theElement.innerHTML = "Ricezione della risposta...";
 	}// if 3
-	else if ( theXhr.readyState === 4 ) {
+	else if (theXhr.readyState === 4) {
 		// verifica della risposta da parte del server
-		if ( theXhr.status === 200 ) {
+		if (theXhr.status === 200) {
 			// operazione avvenuta con successo
 			printResult(theXhr.responseText);
-			
+
 			//location.reload();
 		}// if 200
 
-		 else {
-	        	// errore di caricamento
-			 alert("Impossibile effettuare l'operazione richiesta.");
-//	        	theElement.innerHTML = "Impossibile effettuare l'operazione richiesta.<br />";
-//	        	theElement.innerHTML += "Errore riscontrato: " + theXhr.statusText;
-	        }// else (if ! 200)
+		else {
+			// errore di caricamento
+			alert("Impossibile effettuare l'operazione richiesta.");
+			//	        	theElement.innerHTML = "Impossibile effettuare l'operazione richiesta.<br />";
+			//	        	theElement.innerHTML += "Errore riscontrato: " + theXhr.statusText;
+		}// else (if ! 200)
 	}// if 4
 
 } // callback();
@@ -103,15 +101,15 @@ function callback( theXhr ) {
 function countingIframe() {
 	// qui faccio scaricare al browser direttamente il contenuto del feed come src dell'iframe.
 	Alert("Impossibile effettuare l'operazione, il tuo browser è troppo vecchio")
-	
+
 	// non riesco tuttavia a intervenire per parsificarlo! è il browser che renderizza il src del iframe!
 }// caricaFeedIframe()
 
 
 
-function countingMatrixAJAX( theXhr, mode, aVal, bVal) {
-    
-	
+function countingMatrixAJAX(theXhr, mode, aVal, bVal) {
+
+
 	// impostazione controllo e stato della richiesta
 	theXhr.onreadystatechange = function() { callback(theXhr); };
 	// impostazione richiesta asincrona in GET
@@ -119,14 +117,14 @@ function countingMatrixAJAX( theXhr, mode, aVal, bVal) {
 	try {
 		theXhr.open("post", "MatrixCalculator", true);
 	}
-	catch(e) {
+	catch (e) {
 		// Exceptions are raised when trying to access cross-domain URIs 
 		alert(e);
 	}
 
 	// rimozione dell'header "connection" come "keep alive"
 	//theXhr.setRequestHeader("connection", "close");
-	var argument = "mode="+mode+"&aVal="+aVal+"&bVal="+bVal;
+	var argument = "mode=" + mode + "&aVal=" + aVal + "&bVal=" + bVal;
 	// invio richiesta
 	//
 	theXhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -134,54 +132,46 @@ function countingMatrixAJAX( theXhr, mode, aVal, bVal) {
 
 } // caricaFeedAJAX()
 
-function myFunction(caller)
-{
-	
+function myFunction(caller) {
+
 	var mode = caller.getAttribute("name");
-	
+
 	var aValues = "";
 	var bValues = "";
-	for(var i=0; i<8;i++)
-		{
-			aValues = aValues + myGetElementById("a"+i).value+"@";
-			bValues = bValues + myGetElementById("b"+i).value+"@";
-		}
-	var	xhr = myGetXmlHttpRequest();
+	for (var i = 0; i < 8; i++) {
+		aValues = aValues + myGetElementById("a" + i).value + "@";
+		bValues = bValues + myGetElementById("b" + i).value + "@";
+	}
+	var xhr = myGetXmlHttpRequest();
 
-			if ( xhr ) 
-				countingMatrixAJAX(xhr, mode, aValues, bValues); 
-			else 
-				countingIframe(); 
-		
-	
-		
+	if (xhr)
+		countingMatrixAJAX(xhr, mode, aValues, bValues);
+	else
+		countingIframe();
+
+
+
 
 }
 
-function checkMatrix()
-{
-	var nota= false;
-	var notb= false;
-	for(var i =0; i<8; i++)
-		{
-			var currentValue = myGetElementById("a"+i).value;
-			if(currentValue == '' || isNaN(currentValue))
-				{
-					nota=true;
-				}
+function checkMatrix() {
+	var nota = false;
+	var notb = false;
+	for (var i = 0; i < 8; i++) {
+		var currentValue = myGetElementById("a" + i).value;
+		if (currentValue == '' || isNaN(currentValue)) {
+			nota = true;
 		}
-	for(var i =0; i<8; i++)
-	{
-		var currentValue = myGetElementById("b"+i).value;
-		if(currentValue == '' || isNaN(currentValue))
-			{
-				notb=true;
-			}
 	}
-	
-	if(!notb && !nota)
-		{
+	for (var i = 0; i < 8; i++) {
+		var currentValue = myGetElementById("b" + i).value;
+		if (currentValue == '' || isNaN(currentValue)) {
+			notb = true;
+		}
+	}
+
+	if (!notb && !nota) {
 		var currentValue = myGetElementById("secret");
 		currentValue.style.display = 'block';
-		}
+	}
 }
