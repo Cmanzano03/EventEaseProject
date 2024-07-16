d45c7a02c0cd145c16391ee08a6c6a542e271c58
guille <gubermejo@alumnos.unex.es>
2024-07-16 17:49:52 +0200

    Creacíon algunos ficheros base para el backend y primera edicion clase event

---
+++ Event_Ease_Project/src/bean/Event.java
@@ -0,0 +1,15 @@
+package bean;
+
+public class Event {
+	private String name;
+	private String Description;
+	private String Category;
+	private String Address;
+	private DateTime Date;
+	private int NumberOfTickets;
+	private String TermsAndConditions;
+	private double Price;
+	private String PromotionalCode;
+	
+	
+}
