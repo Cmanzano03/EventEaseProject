d6721f120dcef67c28cae4ce6a829832157e370c
Carlos <cmanzanoo@alumnos.unex.es>
2024-07-17 11:08:04 +0200

    Creado sellticketService, sin hacer aun, hay que crear repositorios

---
+++ Event_Ease_Project/src/bean/SellTicketService.java
@@ -0,0 +1,28 @@
+package bean;
+
+public class SellTicketService {
+	//Repository eventRepository
+	//Repository ticketRepository
+	//Repository userRepository
+	
+	public SellTicketService() {
+		
+	}
+	//Post: Returns the price which have the tickets of the event we are going to buy 
+//	public double getTicketPrice(String eventName){
+//		double price;
+//		
+//		return price;
+//	}
+	//Post: Returns the id of the ticket created
+//	public String createTicket() {
+//		String ticketId;
+//		
+//		return ticketId;
+//	}
+	//Add the ticket created to the user data structures
+//	public void nominateTicketUser() {
+//		
+//	}
+
+}
