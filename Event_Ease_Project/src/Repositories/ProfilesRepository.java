0ed56d038d1b51c334d455f9f5d5ded94272bad2
Carlos <cmanzanoo@alumnos.unex.es>
2024-07-17 14:10:06 +0200

    Repositorios creados, sin implementar aun.

---
+++ Event_Ease_Project/src/Repositories/ProfilesRepository.java
@@ -0,0 +1,23 @@
+package Repositories;
+
+public class ProfilesRepository implements Repository {
+
+	@Override
+	public Repository getRepository() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getElementById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTuple() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Event_Ease_Project/src/Repositories/Repository.java
@@ -0,0 +1,11 @@
+package Repositories;
+
+
+//Interface which defines the basic structure of a repository
+public interface Repository {
+	
+	public Repository getRepository();
+	public 	Object getElementById();
+	public void addTuple();
+	
+}
