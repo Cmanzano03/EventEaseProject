146899ceff4e6093fd59f350ca19cf43a83b07fc
Carlos <cmanzanoo@alumnos.unex.es>
2024-07-21 19:13:23 +0200

    Cambios hechos

--- Event_Ease_Project/web/scripts/login.js
+++ Event_Ease_Project/web/scripts/login.js
@@ -1,14 +1,15 @@
-function myFunction(){
-    
+function myFunction(event){
     var id = document.getElementById("id");
     var password = document.getElementById("password");
     var form = document.getElementById("form");
-    
+
     if(id.value.length == 0 || password.value.length == 0){
-            return
+        // Prevenir el envío del formulario si los campos están vacíos
+        event.preventDefault();
+        alert("Por favor, completa todos los campos.");
+        return;
     }
 
-    //Enviamos los campos
-    form.submit();
-    
+    // No necesitas llamar a form.submit() aquí porque el tipo del botón es submit
+    // y el formulario se enviará automáticamente a menos que preventDefault() sea llamado
 }
