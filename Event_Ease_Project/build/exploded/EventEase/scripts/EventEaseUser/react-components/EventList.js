4e0d6bf859bcc88f04b4f8138669e5bec8feb342
Carlos <cmanzanoo@alumnos.unex.es>
2024-07-22 16:35:20 +0200

    Fin

--- Event_Ease_Project/build/exploded/EventEase/scripts/EventEaseUser/react-components/EventList.js
+++ Event_Ease_Project/build/exploded/EventEase/scripts/EventEaseUser/react-components/EventList.js
@@ -2,43 +2,46 @@
 
 class EventList extends React.Component {
 
-    llamadaAjax() {
+	llamadaAjax(eventId) {
+		const requestData = {
+			eventId: eventId,
+		};
 
-        fetch('http://localhost:8080/EventEase/SellTicketService', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: ""
-        })
-        .then(response => response.json())
-        .then(data => {
-            alert("Ticket successfully acquired");
-            console.log('Ticket:', data);
-        })
-        .catch(error => {
-            console.error('There was an error making the request:', error);
-        });
-    }
+		fetch('http://localhost:8080/EventEase/SellTicket', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestData)
+		})
+			.then(
+				alert("Ticket successfully acquired")
+			)
+			.catch(error => {
+				console.error('There was an error making the request:', error);
+			});
+	}
 
-    render() {
-        // Ensure eventList is an array and has items
-        if (Array.isArray(this.props.eventList) && this.props.eventList.length > 0) {
-            return (
-                <div>
-                    <ul>
-                        {this.props.eventList.map((element) => (
-                            <li  key={element.eventId} style={{ border: '5px solid' }}>
-                                {element.name}
-                                <button onClick={this.llamadaAjax()}>Buy</button>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            );
-        } else {
-            // Return null or some fallback UI if the list is empty or not valid
-            return null;
-        }
-    }
+	render() {
+		if (Array.isArray(this.props.eventList) && this.props.eventList.length > 0) {
+			return (
+				<div>
+					<ul>
+						{this.props.eventList.map((element) => (
+							<li key={element.eventId} style={{ border: '5px solid' }}>
+								{element.name}/
+								{element.city}/
+								{element.date}/
+								{element.numberOfTickets}/
+								{element.price}
+								<button onClick={() => this.llamadaAjax(element.eventId)}>Buy</button>
+							</li>
+						))}
+					</ul>
+				</div>
+			);
+		} else {
+			return null;
+		}
+	}
 }
